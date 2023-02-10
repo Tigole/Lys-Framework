7a4ca7982a6b2572b7571beb0898cd83adfa6626
Tigole <sylvain.janniere@laposte.net>
2023-02-10 11:05:11 +0100

    Improvement

--- include/Lys/MathModule/MathCore.hpp
+++ include/Lys/MathModule/MathCore.hpp
@@ -1,6 +1,10 @@
 #ifndef _LYS_MATH_CORE_HPP
 #define _LYS_MATH_CORE_HPP 1
 
+#include <cmath>
+
 #define POW_2(x) (x*x)
+#define RAD_TO_DEG(rad) (rad * 180.0 / M_PI)
+#define DEG_TO_RAD(deg) (deg * M_PI / 180.0)
 
 #endif // _LYS_MATH_CORE_HPP
